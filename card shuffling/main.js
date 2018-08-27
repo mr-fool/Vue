620bef8eca53c6a0c21b09e7be3e935c1f3dd2b5
mr-fool <mr-fool@users.noreply.github.com>
2018-08-27 14:10:53 -0600

    adding style

--- card shuffling/main.js
+++ card shuffling/main.js
@@ -4,6 +4,15 @@ new Vue({
     ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
     suits: ['♥','♦','♠','♣'],
     cards: [],
+    suitColor: {
+      '♠': 'black',
+      '♣': 'black',
+      '♦': 'red',
+      '♥': 'red',
+    },
+  },
+  created() {
+    this.displayInitialDeck();
   },  
   methods: {
     displayInitialDeck() {
@@ -17,9 +26,16 @@ new Vue({
             rank: this.ranks[r],
             suit: this.suits[s]
           }
-          this.cards.push(card);
-          id++;
-        }
+        	this.cards.push(card);
+			id++;
+    },
+    shuffleDeck() {        
+      for(let i = this.cards.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * i);
+        
+        let temp = this.cards[i];
+        Vue.set(this.cards, i, this.cards[randomIndex]);
+        Vue.set(this.cards, randomIndex, temp);
       }
     },
   },
